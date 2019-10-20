69aa16efcecaabf3ff4e87a42de86786d2f87f03
Marcus Yang <forhappyday@126.com>
2019-10-20 11:56:52 +0800

    2019-10-20 11:56:46

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,22 @@
-var username = 'testusername';
 
+var username = 'testusername';
 username += '！';
+
 
 $(document).ready(function(){
 
     $('.topname').append(username);
+
+    var socket = io.connect('http://localhost:3030');
+
+    socket.on('news', function (data) {
+        console.log(data);
+        socket.emit('my other event', { my: 'data' });
+    });
+
+
+
+
 
 });
 
